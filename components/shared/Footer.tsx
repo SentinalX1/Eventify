71a960e2e41fb2fc49127c503944581a29a17b3a
SentinalX1 <morpheus.val205@gmail.com>
2024-06-23 20:10:11 +0200

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image src="/assets/images/logo.svg" 
+          alt="logo" 
+          height={38} 
+          width={128}/>
+        </Link>
+        <p>&copy; 2024 Eventify. All Rights Reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
