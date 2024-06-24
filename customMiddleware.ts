6d4f933cf67f9466643ac54cc54141a9839a1a71
SentinalX1 <morpheus.val205@gmail.com>
2024-06-24 19:47:00 +0200

    changes

--- customMiddleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export default function customMiddleware(req: NextRequest, event: any) {
-  const url = req.nextUrl;
-  // Your custom logic here
-  return NextResponse.next();
-}
--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,6 @@
-import { clerkMiddleware, ClerkMiddlewareOptions } from "@clerk/nextjs/server";
-import { NextRequest } from 'next/server';
-import customMiddleware from "./customMiddleware";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-export default function middleware(req: NextRequest, event: any) {
-  if (isProduction) {
-    return customMiddleware(req, event);
-  } else {
-    const options: ClerkMiddlewareOptions = {};
-    return clerkMiddleware(options)(req, event);
-  }
-}
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
